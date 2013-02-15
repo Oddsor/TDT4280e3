8269ab9b31440885a85ebe3a7688e25fe494b511
Oddsor <Oddsor@Oddtop>
2013-02-15 14:14:03 +0100

    YEP

--- tdt4280.exercise3/src/agents/TournamentAgent.java
+++ tdt4280.exercise3/src/agents/TournamentAgent.java
@@ -1,21 +1,45 @@
 package agents;
 
 import jade.core.Agent;
+import jade.domain.AMSService;
+import jade.domain.FIPAAgentManagement.AMSAgentDescription;
+import jade.domain.FIPAAgentManagement.SearchConstraints;
+import jade.domain.FIPAException;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @SuppressWarnings("serial")
-public class TournamentAgent extends GeneralAgent {
+public class TournamentAgent extends Agent {
 
-	private ArrayList<Agent> contestants;
-	
-	public TournamentAgent() {
-		contestants = new ArrayList<Agent>();
-	}
-	
-	
-	private void sendScore(){
+    private List<AMSAgentDescription> contestants;
 
-	}
+    @Override
+    public void setup() {
+        contestants = new ArrayList<>();
+        SearchConstraints sc = new SearchConstraints(); 
+        sc.setMaxResults(100L); // not sure of the default value, but this ensures you get them all. 
+        AMSAgentDescription[] evalAgents = null;
+        try {
+            evalAgents = AMSService.search(this, new AMSAgentDescription(), sc);
+        } catch (FIPAException ex) {
+            Logger.getLogger(TournamentAgent.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        for(int i = 0; i < evalAgents.length; i++){
+            if(!evalAgents[i].getName().getLocalName().toString().equals("ams") &&
+                    !evalAgents[i].getName().getLocalName().toString().equals("df") &&
+                    !evalAgents[i].getName().getLocalName().toString().equals("rma") &&
+                    !evalAgents[i].getName().getLocalName().toString().equals(this.getAID().getLocalName())){
+                contestants.add(evalAgents[i]);
+            }
+        }
+        for(AMSAgentDescription a: contestants){
+            System.out.println(a.getName());
+        }
+    }
+	
+	
 
 }
