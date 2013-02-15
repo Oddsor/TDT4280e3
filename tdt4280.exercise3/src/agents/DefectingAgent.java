79707bac1f8f8c60d46111b838a599c6e8b3cf2f
Andreas <andhag@stud.ntnu.no>
2013-02-15 15:31:46 +0100

    Har lagt til rette for å bruke handleMessage()

--- tdt4280.exercise3/src/agents/DefectingAgent.java
+++ tdt4280.exercise3/src/agents/DefectingAgent.java
@@ -1,32 +1,14 @@
 package agents;
 
-import jade.core.behaviours.CyclicBehaviour;
+
 import jade.lang.acl.ACLMessage;
 
 @SuppressWarnings("serial")
 public class DefectingAgent extends GeneralAgent {
 
-	public void setup(){
-		
-		addBehaviour(new CyclicBehaviour() {
-			
-			@Override
-			public void action() {
-//			System.out.println("Checking for new messages");
-			ACLMessage msg = receive();
-				if (msg != null){
-					try {
-						handleMessage(msg);
-						
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		});
-	}
+	
 
-	private void handleMessage(ACLMessage msg){
+	void handleMessage(ACLMessage msg){
 
 		if(msg.getContent() == "DILEMMA"){
 			ACLMessage reply = new ACLMessage(ACLMessage.INFORM);
