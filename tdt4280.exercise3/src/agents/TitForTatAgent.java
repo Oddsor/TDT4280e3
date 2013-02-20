32e9b7299eb71743a6ff7df9f886953538c6fd11
Andreas <andhag@stud.ntnu.no>
2013-02-20 08:11:57 +0100

    Har skrevet tit-for-tat. Ikke testet.

--- tdt4280.exercise3/src/agents/TitForTatAgent.java
+++ tdt4280.exercise3/src/agents/TitForTatAgent.java
@@ -1,16 +1,28 @@
 package agents;
 
+import java.util.ArrayList;
+
 import jade.lang.acl.ACLMessage;
 
 @SuppressWarnings("serial")
 public class TitForTatAgent extends GeneralAgent {
 
-	
+	private boolean otherPlayerDefected = false; //Positive tit-for-tat.
+	ArrayList<String> history = new ArrayList<String>();
+	private final String DEFECT = "DEFECT";
+	private final String COOPERATE = "COOPERATE";
+	private final String DILEMMA = "DILEMMA";
 
 	@Override
 	void handleMessage(ACLMessage msg) {
-		// TODO Auto-generated method stub
+		String message = msg.getContent();
+		if(message.equals(DEFECT)) otherPlayerDefected = true;
+		else if(message.equals(COOPERATE)) otherPlayerDefected = false;
 		
+		if(message.equals(DILEMMA)){
+			
+			sendMessage(msg.getSender(), otherPlayerDefected ? DEFECT : COOPERATE);
+		}
 	}
 
 }
