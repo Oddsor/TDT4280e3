5cb2e0c0df0a05d088d37bf01eae69819289fa18
Andreas <andhag@stud.ntnu.no>
2013-02-21 11:41:55 +0100

    Glemte å legge med RandomAgent.

---
+++ tdt4280.exercise3/src/agents/RandomAgent.java
@@ -0,0 +1,23 @@
+package agents;
+
+
+import jade.lang.acl.ACLMessage;
+/**
+ * 
+ * Agent that chooses by random. Used as a baseline for other agents.
+ *
+ */
+@SuppressWarnings("serial")
+public class RandomAgent extends GeneralAgent {
+
+	private final String DEFECT = "DEFECT";
+	private final String COOPERATE = "COOPERATE";
+
+	@Override
+	void handleMessage(ACLMessage msg){
+
+		if(msg.getContent().equals(DILEMMA))
+			sendMessage(msg.getSender(), Math.random() < 0.5 ? DEFECT : COOPERATE);
+
+	}
+}
