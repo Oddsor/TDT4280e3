8d3ad24be29929012b3ebec88170dbcd4e2e3e9f
Odd <Odd@Odd-PC>
2013-03-07 14:03:47 +0100

    -SolverAgent now has a rudimentary control flow -ReplyBehaviour now sends Inform-messages and tells the agent to remove it from the list of remaining tasks.

--- tdt4280.exercise3/src/exercise4/SolverAgent.java
+++ tdt4280.exercise3/src/exercise4/SolverAgent.java
@@ -1,35 +1,38 @@
 package exercise4;
 
-import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
-import jade.core.behaviours.DataStore;
-import jade.core.behaviours.WakerBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
+import java.util.LinkedList;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  * Abstract class for handling general communication
+ *
  * @author Andreas
  *
  */
-
 @SuppressWarnings("serial")
 public abstract class SolverAgent extends Agent {
-    private WakerBehaviour delayedReply;
-    private int replySpeed = 0;
+
+    private int replySpeed = 1;
     
+    ACLMessage receivedProblem;
+    
+    LinkedList<ReplyBehaviour> replyList;
+
     /**
      * Here we register the agent's protocol in the DF service thingamajig.
-     * @param operand
+     *
+     * @param operator
      */
-    public void registerService(String operator){
+    public void registerService(String operator) {
         DFAgentDescription dfa = new DFAgentDescription();
         dfa.addLanguages("MATH LOL");
         ServiceDescription sd = new ServiceDescription();
@@ -43,91 +46,97 @@ public abstract class SolverAgent extends Agent {
         }
     }
 
-	public void sendMessage(jade.core.AID receiver, String content){
-		ACLMessage msg = new ACLMessage(ACLMessage.CFP);
-		msg.addReceiver(receiver);
-		msg.setContent(content);
-		send(msg);
-	}
+    public void sendMessage(jade.core.AID receiver, String content, int type) {
+        ACLMessage msg = new ACLMessage(type);
+        msg.addReceiver(receiver);
+        msg.setContent(content);
+        send(msg);
+    }
 
-	public void sendMessage(jade.core.AID receiver, String content, int type){
-		ACLMessage msg = new ACLMessage(type);
-		msg.addReceiver(receiver);
-		msg.setContent(content);
-		send(msg);
-	}
-
-	//TODO: Add ability to wait through WakerBehaviour.
-	@Override
-	public void setup(){
-            Random rand = new Random();
-            replySpeed = rand.nextInt(4) + 1;
-            
-            //TODO: if operator-variable isn't updated upwards (from i.e. multiplication solver to solveragent), then fix.
-		addBehaviour(new CyclicBehaviour(this) {
-			public Agent getMyAgent() {
-				return myAgent;
-			}
-			@Override
-			public void action() {
-				SolverAgent ag = (SolverAgent) getMyAgent();
-				ACLMessage msg = blockingReceive();
-				if (msg != null){
-					try {
-						ag.handleMessage(msg);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		});
-	}
-	
-	private void handleMessage(ACLMessage msg){
-		switch (msg.getPerformative()) {
-		case ACLMessage.QUERY_IF:
-
-			String msgArray[] = msg.getContent().split(",");
-			double x = Double.parseDouble(msgArray[0]);
-			double y = Double.parseDouble(msgArray[1]);
-			sendMessage(msg.getSender(), solve(x,y));
-			System.out.println(solve(x,y));
-			break;
-		case ACLMessage.CFP:
-			
-			System.out.println("Call for proposal received: "+msg.getContent());
-			handleCFP(msg);
-                    break;
-                case ACLMessage.ACCEPT_PROPOSAL:
-                    delayedReply = new ReplyBehaviour(this, replySpeed * 1000, msg);
-                    addBehaviour(delayedReply);
-		default:
-			break;
-		}
-	}
-	abstract String solve(double x, double y);
+    @Override
+    public void setup() {
+        //We may receive more problems and thus end up with a queue of behaviours!
+        replyList = new LinkedList<ReplyBehaviour>();
         
-        /**
-         * We expect messages in the form of "5 + 5"
-         * @param msg 
-         */
-	abstract void handleCFP(ACLMessage msg);
-        
-        /**
-         * Put 
-         * @param receiver
-         * @param answer
-         * @param delay 
-         */
-        private void delayedReply(AID receiver, double answer,int delay){
-            ACLMessage reply = new ACLMessage(ACLMessage.INFORM);
-            reply.addReceiver(receiver);
-            reply.setContent("" + answer);
-            
-            DataStore ds = new DataStore();
-            ds.put("msg", reply);
+        addBehaviour(new CyclicBehaviour(this) {
+            public Agent getMyAgent() {
+                return myAgent;
+            }
+
+            @Override
+            public void action() {
+                SolverAgent ag = (SolverAgent) getMyAgent();
+                ACLMessage msg = blockingReceive();
+                if (msg != null) {
+                    try {
+                        ag.handleMessage(msg);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    private void handleMessage(ACLMessage msg) {
+        switch (msg.getPerformative()) {
+            case ACLMessage.QUERY_IF:
+
+                String msgArray[] = msg.getContent().split(",");
+                double x = Double.parseDouble(msgArray[0]);
+                double y = Double.parseDouble(msgArray[1]);
+                sendMessage(msg.getSender(), solve(x, y), ACLMessage.CFP);
+                System.out.println(solve(x, y));
+                break;
+            case ACLMessage.CFP:
+                System.out.println("Call for proposal received: " + msg.getContent());
+                receivedProblem = msg;
+                bid(msg);
+                break;
+            case ACLMessage.ACCEPT_PROPOSAL:
+                //Proposal was accepted, solve new problem.
+                replyList.add(new ReplyBehaviour(this, getRelativeSpeed(), receivedProblem));
+                addBehaviour(replyList.getLast());
+                break;
+            default:
+                break;
         }
+    }
+    
+    public void bid(ACLMessage msg){
+        String bidString = "bid(";
+        bidString += (getRelativeSpeed() / 1000) + " sec)";
+        sendMessage(msg.getSender(), bidString, ACLMessage.PROPOSE);
+    }
+    
+    public void problemSolved(ReplyBehaviour problem){
+        replyList.remove(problem);
+    }
+    
+    /**
+     * Here we take the "normal" time of replySpeed and add up the total wait
+     * time that is left in all remaining behaviours.
+     * @return 
+     */
+    public int getRelativeSpeed(){
+        int relativeSpeed = replySpeed * 1000;
+        
+        if(replyList.size() > 0){
+            for (int i = 0; i < replyList.size(); i++){
+                replySpeed += (int) replyList.get(i).getWakeupTime();
+            }
+        } 
+        return relativeSpeed;
+    }
+
+    abstract String solve(double x, double y);
+    
+
+    /**
+     * We expect messages in the form of "5 + 5"
+     * 
+     * @deprecated Should not need this anymore.
+     * @param msg
+     */
+    abstract void handleCFP(ACLMessage msg);
 }
-
-
-
